4ad7d7f4926652cdd60fbb23ebf66e3c75c4f658
Zahra Janssens <zzahra.janssens@telenet.be>
2023-01-13 20:19:37 +0100

    scherm toegevoegd

--- screens/winkelkar.js
+++ screens/winkelkar.js
@@ -3,3 +3,22 @@
 
 // import React, {useState, useEffect} from 'react';
 // import {StyleSheet, Text, View, Image, TextInput, Pressable, FlatList,fontFamily} from 'react-native';
+
+const Winkelkarretje = props =>  ({route, navigation})=>{
+
+
+    // return (
+    //   <Pressable onPress={() => navigation.navigate("Winkelkar")}>
+    //       <View style={styles.shoppingcar}>
+    //           <Image 
+    //               style={styles.icon}
+    //               source={require("..assets/shopping-cart.png")}
+    //           />
+    //           <Text style={styles.getal}>0</Text>
+    //       </View>
+    //   </Pressable>
+    // );
+  
+}
+
+export default Winkelkarretje;
