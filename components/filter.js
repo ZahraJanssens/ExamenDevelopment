93832663e10ad87b97caa587d0327ee3cedc02e4
Zahra Janssens <zzahra.janssens@telenet.be>
2023-01-13 17:26:43 +0100

    filter toegevoegd

---
+++ components/filter.js
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import React, {useState} from 'react';
+
+const Filter = props => {
+  const [pressCounter, setPressCounter] = useState (0)
+  function pressHandler() {
+    console.log("pressed" + props.title);
+    setPressCounter((currentPressCounter)=> currentPressCounter + 1);
+  }
+  return (
+    <TouchableOpacity style={styles.tile} activeOpacity={0.7} onPress={pressHandler}>
+      {console.log("render " + props.title)}
+      <View>
+        <Text style={styles.tileHeader}>{props.title}</Text>
+        <Text style={styles.tileText}>{pressCounter}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+
+}
+
+export default Filter;
