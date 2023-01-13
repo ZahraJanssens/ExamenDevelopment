6ffd33082c246a4fa245076a9c589c963a7b9a8b
Zahra Janssens <zzahra.janssens@telenet.be>
2023-01-13 17:31:30 +0100

    winkelkar toegevoegd

---
+++ screens/winkelkar.js
@@ -0,0 +1,5 @@
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// import React, {useState, useEffect} from 'react';
+// import {StyleSheet, Text, View, Image, TextInput, Pressable, FlatList,fontFamily} from 'react-native';
