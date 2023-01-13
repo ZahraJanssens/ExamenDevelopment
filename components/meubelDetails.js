8c0585d9e1cf66d4eb574f3be25c26e488fdb5c5
Zahra Janssens <zzahra.janssens@telenet.be>
2023-01-13 17:28:31 +0100

    component van gemaakt

--- components/meubelDetails.js
+++ components/meubelDetails.js
@@ -1,90 +1,22 @@
-// import React, { useState, useEffect } from 'react';
-// import { View, Text, StyleSheet, Image, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import React, {useState} from 'react';
 
-// import apiKey from '../apiKey';
-// import meubels from '../screens/meubels';
+const Details = props => {
+  const [pressCounter, setPressCounter] = useState (0)
+  function pressHandler() {
+    console.log("pressed" + props.title);
+    setPressCounter((currentPressCounter)=> currentPressCounter + 1);
+  }
+  return (
+    <TouchableOpacity style={styles.tile} activeOpacity={0.7} onPress={pressHandler}>
+      {console.log("render " + props.title)}
+      <View>
+        <Text style={styles.tileHeader}>{props.title}</Text>
+        <Text style={styles.tileText}>{pressCounter}</Text>
+      </View>
+    </TouchableOpacity>
+  );
 
+}
 
-// const meubels = ({navigation}) =>{
-
-//     const [meubels, setMeubels] = useState ([]);
-
-//     const getMeubels = async () => {
-//         try {
-//             const response = await fetch ("http://zahrajanssens.be/wp-json/wp/v2/posts?categories=6", {
-
-//             })
-//             const json = await response.json();
-//             setMeubels(json);
-//             console.log(meubels);
-//         } catch (error) {
-//             console.error(error);
-//         }
-//     }
-
-//     useEffect(() => {
-//         getMeubels();
-//     }, []);
-
-
-//     return (
-//         <View>
-//             <FlatList data={meubels} renderItem={({item}) => (
-                
-//                 <View>
-//                 <Text>{item.title.rendered}</Text>
-            
-//             <Pressable onPress ={()=> navigation.navigate ("info", {itemTitle: item.title.rendered})}>
-                
-//                 <View style={styles.listItem}>
-//                     <Text>Bekijk product:</Text>
-//                 </View>
-
-//             </Pressable>
-
-//                 <ScrollView>
-//                     <Image
-//                         style={styles.afbeelding}
-//                         source={{
-//                         uri: meubelDetails.banner,
-//                     }}
-//                     />
-//                     <Text style={styles.listItem}>{meubelDetails.title}</Text>
-//                         <View style={styles.details}>
-//                             <Text>{meubleDetails.plot}</Text>
-//                             <Text style={styles.release}>release: {meubelDetails.release}</Text>
-//                         </View>
-
-//                 </ScrollView>
-
-
-//             </View>
-//             )}/>
-//         </View>
-//     )   
-// }
-
-// const styles = StyleSheet.create({
-//   listItem: {
-//     margin: 16,
-//     fontSize: 24,
-//     textAlign: 'center',
-//   },
-//   details: {
-//     borderWidth: 1,
-//     padding: 16,
-//     margin: 8,
-//   },
-//   afbeelding: {
-//     width: '100%',
-//     height: 450
-//   },
-//   release: {
-//     fontStyle: 'italic',
-//     fontSize: 12,
-//     marginTop: 8,
-//     textAlign: 'right',
-//   }
-
-// });
-// export default meubelDetails;
+export default MeubelDetails;
